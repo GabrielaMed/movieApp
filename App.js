2b9ac93a01f86dfc111cc90ba209c930b2d59230
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-06-16 15:39:41 -0400

    changing route form

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native';
-
-import Routes from './src/routes'
+import { NavigationContainer } from '@react-navigation/native'
+import { useState } from 'react'
+import { Home } from './src/pages/Home'
+import { Details } from './src/pages/Details'
 
 export default function App() {
+  const [globalState, setGlobalState] = useState({ page: 'home', movieId: 0 })
+
   return (
     <NavigationContainer>
-      <Routes/>
+      {globalState.page === 'home' && (
+        <Home prop={{ globalState, setGlobalState }} />
+      )}
+      {globalState.page === 'detail' && (
+        <Details prop={{ globalState, setGlobalState }} />
+      )}
     </NavigationContainer>
-  );
+  )
 }
